a0f4804a960751e73956e3e5acada9fbe1abe58e
asalua181822128024 <144455136+asalua181822128024@users.noreply.github.com>
2023-09-08 19:18:32 +0530

    Create Suba.py

---
+++ Suba.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+def is leapyear(year):
+   if(year % 4==0 and year % 100!=0) or year%400
+    ==0:
+     return true
+   else:
+      return False
+  year=int (input ("enter a year :"))
+
+  if is leap year( year):
+    print('{} is not a leap year.'. format ( year))
+else:
+    print('{} is not a leap year.'.format(year))
+          
